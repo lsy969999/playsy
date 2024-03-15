137e164e5a4c42e10ace61d29e83937f6bf71779
lsy969999 <lsy969999@gmail.com>
2024-03-16 05:44:26 +0900

    warn 제거

--- infra/pulumi/index.ts
+++ infra/pulumi/index.ts
@@ -105,7 +105,7 @@ apt-get install docker-ce docker-ce-cli containerd.io -y
 apt-get install jq
 VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
 DESTINATION=/usr/bin/docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+sudo curl -L https://github.com/docker/compose/releases/download/$VERSION/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
 sudo chmod 755 $DESTINATION
 
 # npm 설치
